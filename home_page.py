f63d8bc12066c4f81741f0a9db739e8258b42f40
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-08-07 20:59:48 -0400

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -2,5 +2,11 @@
 import streamlit as st
 
 def home_page():
-    st.title("Welcome to the Program")
-    st.write("This program is designed to ...") # Provide a description of the program
+    st.title("Welcome to My App")
+    st.write("This is the home page of the application. You can navigate to other pages from here:")
+
+    if st.button("Go to Use Page"):
+        st.session_state.page = 'use'
+
+    if st.button("Go to Terms of Use Page"):
+        st.session_state.page = 'terms'
