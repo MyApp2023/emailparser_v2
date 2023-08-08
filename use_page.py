1ed4e878678f7bc7940c48762b7050d2db396afa
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-08-07 20:33:11 -0400

    Update use_page.py

--- use_page.py
+++ use_page.py
@@ -1,5 +1,85 @@
-
 import streamlit as st
+
+def use_page():
+    # Existing code
+import streamlit as st
+import googlemaps
+import requests
+import re
+import hashlib
+import time
+
+MAX_URLS = 50
+
+# Counter for generating unique widget keys
+widget_counter = 0
+
+def get_unique_key():
+    global widget_counter
+    widget_counter += 1
+    return f"widget_{widget_counter}"
+
+def read_config_file():
+    config = {}
+    with open("config.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            key, value = line.strip().split("=")
+            config[key] = value
+    return config
+
+def get_place_urls(query, num_results, api_key):
+    gmaps = googlemaps.Client(key=api_key)
+    response = gmaps.places(query=query)
+    results = []
+    for place in response['results']:
+        place_details = gmaps.place(place_id=place['place_id'], fields=['website'])
+        if 'website' in place_details['result']:
+            results.append(place_details['result']['website'])
+        if len(results) == num_results:
+            break
+    return results[:num_results]
+
+def get_search_results(query, num_results, api_key, search_engine_id):
+    url = f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}'
+    response = requests.get(url)
+    data = response.json()
+    items = data.get('items', [])
+    results = [item['link'] for item in items[:num_results]]
+    return results[:num_results]
+
+def find_email_addresses(urls, max_emails):
+    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+    email_addresses = {}
+    for i, url in enumerate(urls, start=1):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                email_matches = re.findall(email_regex, response.text)
+                if email_matches:
+                    filtered_emails = set()  # Use a set to remove duplicates
+                    for email in email_matches:
+                        if not email.endswith(("wixpress.com", "sentry.io", ".png", "jpg", ".html")):
+                            filtered_emails.add(email)
+                    if filtered_emails:
+                        email_addresses[url] = list(filtered_emails)[:max_emails]  # Limit the number of emails
+                    else:
+                        email_addresses[url] = ['Email not found']
+                else:
+                    email_addresses[url] = ['Email not found']
+        except requests.exceptions.RequestException as e:
+            st.write(f"Error retrieving content from {url}: {e}")
+        if i == MAX_URLS:
+            break
+    return email_addresses
+
+
+# Read API keys and search engine ID from config.txt
+config = read_config_file()
+google_maps_api_key = config.get("GOOGLE_MAPS_API_KEY", "")
+google_search_api_key = config.get("GOOGLE_SEARCH_API_KEY", "")
+search_engine_id = config.get("SEARCH_ENGINE_ID", "")
+
 st.title("Email Parser")
 
 # Prompt for search input
