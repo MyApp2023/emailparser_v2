802cc54f9257d4e99cbfc73b03f14b2eccc82f17
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-08-07 21:02:06 -0400

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -5,8 +5,6 @@ def home_page():
     st.title("Welcome to My App")
     st.write("This is the home page of the application. You can navigate to other pages from here:")
 
-    if st.button("Go to Use Page"):
+    if st.button("Try it now"):
         st.session_state.page = 'use'
 
-    if st.button("Go to Terms of Use Page"):
-        st.session_state.page = 'terms'
