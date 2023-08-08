70949a85b250210ca55d22339fc6c7214a3b79f6
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-08-07 20:54:39 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,10 +3,22 @@ from home_page import home_page
 from use_page import use_page
 from terms_of_use_page import terms_of_use_page
 
+# Initialize session state for navigation
+if 'page' not in st.session_state:
+    st.session_state.page = 'home'
+
 st.sidebar.title("Navigation")
 if st.sidebar.button("Home"):
-    home_page()
+    st.session_state.page = 'home'
 if st.sidebar.button("Use"):
-    use_page()
+    st.session_state.page = 'use'
 if st.sidebar.button("Terms of Use"):
+    st.session_state.page = 'terms'
+
+# Display the corresponding page
+if st.session_state.page == 'home':
+    home_page()
+elif st.session_state.page == 'use':
+    use_page()
+elif st.session_state.page == 'terms':
     terms_of_use_page()
