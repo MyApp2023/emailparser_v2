b993525d923307796a8c52f178bb5e63ff41011c
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-08-12 20:49:17 -0400

    Update terms_of_use_page.py

--- terms_of_use_page.py
+++ terms_of_use_page.py
@@ -1,6 +1,42 @@
-
 import streamlit as st
 
 def terms_of_use_page():
     st.title("Terms of Use")
-    st.write("By using this program, you agree to the following terms: ...") # Provide the terms of use
+    
+    st.write(
+        "1. **Acceptance of Terms**\n\n"
+        "By accessing or using the FindMail application (\"the App\"), you agree to be bound "
+        "by these Terms of Use (\"Terms\"). If you do not agree to these Terms, please do not use the App.\n\n"
+        
+        "2. **Description of Service**\n\n"
+        "FindMail provides users with tools to search for and retrieve email addresses from various "
+        "websites. The App is intended for personal and business use to facilitate communication.\n\n"
+        
+        "3. **User Conduct**\n\n"
+        "You agree to use the App for lawful purposes only. You must not use the App to harvest email "
+        "addresses for spamming or any other illegal or unethical activities.\n\n"
+        
+        "4. **Limitation of Liability**\n\n"
+        "FindMail is not responsible for any damages, losses, or issues arising from the use of the App, "
+        "including but not limited to incorrect email addresses, miscommunication, or any other results of using the App.\n\n"
+        
+        "5. **Privacy**\n\n"
+        "Your privacy is important to us. Please review our Privacy Policy, which outlines how we handle "
+        "and protect your personal data.\n\n"
+        
+        "6. **Changes to the Terms**\n\n"
+        "FindMail reserves the right to modify or update these Terms at any time without prior notice. "
+        "It is the user's responsibility to review the Terms regularly.\n\n"
+        
+        "7. **Termination**\n\n"
+        "FindMail reserves the right to terminate or restrict access to the App for any reason, without notice.\n\n"
+        
+        "8. **Governing Law**\n\n"
+        "These Terms are governed by the laws of the jurisdiction in which FindMail operates. Any disputes arising "
+        "from these Terms will be resolved in the respective jurisdiction's courts.\n\n"
+        
+        "9. **Contact**\n\n"
+        "For any questions or concerns regarding these Terms, please contact us at [contact@findmail.com](mailto:contact@findmail.com).\n\n"
+        
+        "By using FindMail, you acknowledge that you have read, understood, and agree to be bound by these Terms of Use."
+    )
